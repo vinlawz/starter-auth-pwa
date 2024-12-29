2b0bb013e4e34f2c9df89ec25a2dafaeb1b1f957
ca <achinga.chris@gmail.com>
2024-12-30 01:57:36 +0300

    add: user model
    custom user models and a user manager for creating superuser and users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class User(AbstractUser, PermissionsMixin):
+    username = None
+    email = models.EmailField(
+        _("email address"), 
+        unique=True,
+        db_index=True
+    )
+    
+    first_name = models.CharField(_("first name"), max_length=30, blank=True)
+    last_name = models.CharField(_("last name"), max_length=30, blank=True)
+    is_verified = models.BooleanField(
+        default=False,
+        help_text=_("Designates whether this user has verified their email.")
+    )
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
